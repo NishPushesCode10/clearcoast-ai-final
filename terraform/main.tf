49f426deb79d8d7e295b2ed9aaa2a9107018a3b0
Nishanth Posa <nishanth.posa10@gmail.com>
2026-05-12 23:00:46 +0530

    Add Azure resources for web app deployment

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+# ===== main.tf =====
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+  tags                = var.tags
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+    always_on = false
+    ftps_state = "Disabled"
+  }
+
+  https_only = true
+  tags       = var.tags
+}
+
+resource "azurerm_application_insights" "insights" {
+  name                = "${var.app_service_name}-insights"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = "web"
+  tags                = var.tags
+}
